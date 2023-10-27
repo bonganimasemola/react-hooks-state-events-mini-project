1a3419640defc4308c4b60151b7c6b3c06940d29
bonganimasemola <bonganimasemola@icloud.com>
2023-10-27 20:31:30 +0300

    frustrating as hell

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,13 +1,23 @@
 import React from "react";
+import { CATEGORIES } from "../data";
 
-function CategoryFilter() {
-  
+function CategoryFilter({ selectedCategory, handleCategoryClick }) {
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {CATEGORIES.map((category, index) => (
+        <button
+          key={index}
+          className={`category-button ${selectedCategory === category ? "selected" : ""}`}
+          onClick={() => handleCategoryClick(category)}
+        >
+          {category}
+        </button>
+      ))}
     </div>
   );
 }
 
 export default CategoryFilter;
+
+
